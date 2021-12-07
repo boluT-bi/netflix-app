6a067a288f06820827bbdec57c57c6530fd82b59
ghost <ghostalabi95@gmail.com>
2021-12-07 14:10:49 +0000

    config for heroku postgres connection

--- bckEnd/settings/prod.py
+++ bckEnd/settings/prod.py
@@ -2,6 +2,8 @@ from .common import *
 import os
 
 DEBUG = False
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*',]
 ROOT_URI = 'https://ntflx-app.heroku.com/'
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
